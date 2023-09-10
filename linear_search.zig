19876a8d813f7efc3e0b1a21fe25637459186d0e
Julian Gomez <julngomz@gmail.com>
2023-09-10 17:35:15 -0500

    update linear search

--- linear_search.zig
+++ linear_search.zig
@@ -24,8 +24,8 @@ test "Linear Search" {
 // Generic Linear Search
 pub fn g_linear_search(
     comptime T: type,
-    haystack: T, 
-    needle: anytype
+    haystack: []const T, 
+    needle: T
 ) bool {
     for (haystack) |value| {
         if (value == needle) return true;
@@ -38,16 +38,16 @@ test "Generic Linear Search" {
     // String
     var word: []const u8 = "hello";
     
-    try expect(g_linear_search([]const u8, word, 'h') == true);
-    try expect(g_linear_search([]const u8, word, 'g') == false);
-    try expect(g_linear_search([]const u8, word, 3) == false);
+    try expect(g_linear_search(u8, word, 'h') == true);
+    try expect(g_linear_search(u8, word, 'g') == false);
+    try expect(g_linear_search(u8, word, 3) == false);
 
     // Integers
     var numbers = [_]u8{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     for (0..10) |i| {
-        try expect(g_linear_search([]u8, &numbers, @as(u8, @truncate(i))) == true);
+        try expect(g_linear_search(u8, &numbers, @as(u8, @truncate(i))) == true);
     }
 
-    try expect(g_linear_search([]u8, &numbers, 10) == false);
+    try expect(g_linear_search(u8, &numbers, 10) == false);
 }
