7543ffe9f1983e960b1d79302dbf3516484e1119
Julian Gomez <julngomz@gmail.com>
2023-09-10 18:07:33 -0500

    update linear search

--- linear_search.zig
+++ linear_search.zig
@@ -3,7 +3,7 @@ const testing = std.testing;
 const expect = testing.expect;
 
 // Generic Linear Search
-pub fn g_linear_search(
+pub fn linear_search(
     comptime T: type,
     haystack: []const T, 
     needle: T
@@ -19,16 +19,16 @@ test "Generic Linear Search" {
     // String
     var word: []const u8 = "hello";
     
-    try expect(g_linear_search(u8, word, 'h') == true);
-    try expect(g_linear_search(u8, word, 'g') == false);
-    try expect(g_linear_search(u8, word, 3) == false);
+    try expect(linear_search(u8, word, 'h') == true);
+    try expect(linear_search(u8, word, 'g') == false);
+    try expect(linear_search(u8, word, 32) == false);
 
     // Integers
     var numbers = [_]u8{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     for (0..10) |i| {
-        try expect(g_linear_search(u8, &numbers, @as(u8, @truncate(i))) == true);
+        try expect(linear_search(u8, &numbers, @as(u8, @truncate(i))) == true);
     }
 
-    try expect(g_linear_search(u8, &numbers, 10) == false);
+    try expect(linear_search(u8, &numbers, 10) == false);
 }
